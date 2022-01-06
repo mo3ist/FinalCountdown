edb3fce56eea06ed265e223075efcc976314bd1d
mo3ist <50373064+mo3ist@users.noreply.github.com>
2022-01-07 01:33:43 +0200

    fix: stop using whitenoise

--- conf/wsgi.py
+++ conf/wsgi.py
@@ -15,5 +15,3 @@ from whitenoise import WhiteNoise
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'conf.settings')
 
 application = get_wsgi_application()
-
-application = WhiteNoise(application, root='static/')
